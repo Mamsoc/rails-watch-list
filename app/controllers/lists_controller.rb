6859412104ea9072f89606823b71d6defc78a3f2
Marion Socha <marion.socha@protonmail.com>
2022-05-19 17:12:49 +0200

    bookmark creation

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
